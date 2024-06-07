9ee38c72cbaf0e08af8142b027bde7eccf568b42
rickshaw68 <rickshaw@myyahoo.com>
2024-06-06 19:09:01 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+
+
+            // Console.Write("What is the magic number? ");
+            // string numberText = Console.ReadLine();           
+
+            // int number = int.Parse(numberText);
+            int guess = 0; 
+            int count = 1;       
+
+            while (guess != number)           
+            {   
+                Console.Write("What is your guess? ");
+                string guessText = Console.ReadLine();
+                guess = int.Parse(guessText);
+                
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                    count += 1;                               
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                    count += 1;                 
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed the number in {count} tries.");
+                }                
+            }
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine(); 
+        }   while (response == "yes");
+        
     }
 }
