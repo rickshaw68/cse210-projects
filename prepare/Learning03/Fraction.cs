1fa6ab739d9fa4cbfcb3191291ad8c431051c388
rickshaw68 <rickshaw@myyahoo.com>
2024-06-20 21:33:57 -0600

    Completed Learning 03 Fractions

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,45 @@
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public int Top
+    {
+        get {return _numerator; }
+        set {_numerator = value; }
+    }
+
+    public int Bottom
+    {
+        get {return _denominator; }
+        set {_denominator = value; }
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction F1 = new Fraction();
+        Console.WriteLine(F1.GetFractionString());
+        Console.WriteLine(F1.GetDecimalValue());
+
+        Fraction F2 = new Fraction(6);
+        Console.WriteLine(F2.GetFractionString());
+        Console.WriteLine(F2.GetDecimalValue());
+
+        Fraction F3 = new Fraction(6,7);
+        Console.WriteLine(F3.GetFractionString());
+        Console.WriteLine(F3.GetDecimalValue());
+
+        Fraction F4 = new Fraction(3,4);
+        Console.WriteLine(F4.GetFractionString());
+        Console.WriteLine(F4.GetDecimalValue());
+
+        F4.Top = 7; // my getters and setters
+        F4.Bottom = 5;
+        Console.WriteLine(F4.GetFractionString());
+        Console.WriteLine(F4.GetDecimalValue());
     }
 }
